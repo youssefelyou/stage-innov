4e88867c186925eda0393d5f045926a3364501e3
youssefelyou <youssefelyourizi36@gmail.com>
2023-07-19 11:21:34 +0100

    Initial commit

---
+++ src/main/java/com/example/stageinnov/StageInnovApplication.java
@@ -0,0 +1,13 @@
+package com.example.stageinnov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageInnovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageInnovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stageinnov/StageInnovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stageinnov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageInnovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
