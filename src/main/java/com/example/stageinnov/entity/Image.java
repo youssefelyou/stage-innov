cd869e254f7e3bbb1e43af1474aae5f45bcf9692
youssefelyou <youssefelyourizi36@gmail.com>
2023-07-25 12:07:51 +0100

    Image updated

--- src/main/java/com/example/stageinnov/entity/Image.java
+++ src/main/java/com/example/stageinnov/entity/Image.java
@@ -1,19 +1,23 @@
 package com.example.stageinnov.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String img;
+    private String photo;
     private String description;
     private String Format;
     @ManyToOne
     private Projet projet;
-    @ManyToOne
-    private User user;
 
 
 }
