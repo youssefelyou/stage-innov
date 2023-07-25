6c7ad1af69f324d76c03ed3a177363a69bb783ff
youssefelyou <youssefelyourizi36@gmail.com>
2023-07-25 11:12:34 +0100

    Image updated

--- src/main/java/com/example/stageinnov/repository/ImageRepository.java
+++ src/main/java/com/example/stageinnov/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.example.stageinnov.repository;
 
 import com.example.stageinnov.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Integer> {
 }
